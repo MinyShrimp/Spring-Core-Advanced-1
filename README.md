6a9e9d3a7bded7a329dc9eb56ef0917d03f7a7ac
MinyShrimp <ksk7584@gmail.com>
2023-03-01 01:04:09 +0900

    스프링 핵심 원리 - 고급편 프로젝트 생성

---
+++ README.md
@@ -0,0 +1,12 @@
+# 스프링 핵심 원리 - 고급편
+
+* 강의:
+  [김영한님의 스프링 핵심 원리 - 고급편](https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B3%A0%EA%B8%89%ED%8E%B8/dashboard)
+* 깃헙:
+    * https://github.com/MinyShrimp/Spring-Core-Advanced-1
+
+## 예제 만들기
+
+## 쓰레드 로컬 - ThreadLocal
+
+## 템플릿 메서드 패턴과 콜백 패턴
---
+++ src/main/java/hello/springcoreadvanced1/SpringCoreAdvanced1Application.java
@@ -0,0 +1,13 @@
+package hello.springcoreadvanced1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreAdvanced1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreAdvanced1Application.class, args);
+	}
+
+}
